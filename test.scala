e1b974b373d354f91998f850570ef64f63240770
Amy <adam.thomas.rogerson@gmail.com>
2016-10-09 15:15:56 +0100

    test

---
+++ test.scala
@@ -0,0 +1,21 @@
+object test{
+
+  def main(args: Array[String]) =
+  {
+      println(wordJoiner("adam","amy"))
+
+    def wordJoiner(x:String, y:String):String ={
+
+      def wordStich(x:String,y:String,stiched:String)={
+
+       // if (x.length < stiched.length)
+
+      }
+
+
+
+    }//end wordjoiner
+
+  }//end main
+
+}//end test
